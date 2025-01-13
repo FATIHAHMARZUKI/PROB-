95b2434136aee266c505af1a50dedd192f3ea309
Fatihahmrzuki <fatihahmarzuki1211@gmail.com>
2025-01-14 01:36:19 +0800

    Update depress.py

--- depress.py
+++ depress.py
@@ -9,15 +9,15 @@ st.write("This prototype aims to provide a self-assessment and educational tool 
 # Depression Screening - PHQ-9
 st.header("Depression Screening Questionnaire (PHQ-9)")
 questions_depression = [
-    "Little interest or pleasure in doing things?",
-    "Feeling down, depressed, or hopeless?",
-    "Trouble falling or staying asleep, or sleeping too much?",
-    "Feeling tired or having little energy?",
-    "Poor appetite or overeating?",
-    "Feeling bad about yourself, or that you are a failure or have let yourself or your family down?",
-    "Trouble concentrating on things, such as reading the newspaper or watching television?",
-    "Moving or speaking so slowly that other people could have noticed? Or the opposite — being so fidgety or restless that you have been moving around a lot more than usual?",
-    "Thoughts that you would be better off dead, or of hurting yourself in some way?"
+    "1. Little interest or pleasure in doing things?",
+    "2. Feeling down, depressed, or hopeless?",
+    "3. Trouble falling or staying asleep, or sleeping too much?",
+    "4. Feeling tired or having little energy?",
+    "5. Poor appetite or overeating?",
+    "6. Feeling bad about yourself, or that you are a failure or have let yourself or your family down?",
+    "7. Trouble concentrating on things, such as reading the newspaper or watching television?",
+    "8. Moving or speaking so slowly that other people could have noticed? Or the opposite — being so fidgety or restless that you have been moving around a lot more than usual?",
+    "9. Thoughts that you would be better off dead, or of hurting yourself in some way?"
 ]
 responses_depression = []
 for question in questions_depression:
@@ -41,13 +41,13 @@ elif score_depression >= 5:
 # Anxiety Screening - GAD-7
 st.header("Anxiety Screening Questionnaire (GAD-7)")
 questions_anxiety = [
-    "Feeling nervous, anxious, or on edge?",
-    "Not being able to stop or control worrying?",
-    "Worrying too much about different things?",
-    "Trouble relaxing?",
-    "Being so restless that it's hard to sit still?",
-    "Becoming easily annoyed or irritable?",
-    "Feeling afraid as if something awful might happen?"
+    "1. Feeling nervous, anxious, or on edge?",
+    "2. Not being able to stop or control worrying?",
+    "3. Worrying too much about different things?",
+    "4. Trouble relaxing?",
+    "5. Being so restless that it's hard to sit still?",
+    "6. Becoming easily annoyed or irritable?",
+    "7. Feeling afraid as if something awful might happen?"
 ]
 responses_anxiety = []
 for question in questions_anxiety:
